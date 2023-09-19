c46aeace3f2682ff359ecbc1f2fe77a1e95916f9
Dave Roberts <dave@droberts.com>
2023-09-19 14:56:42 -0500

    Update version in build.clj

--- build.clj
+++ build.clj
@@ -4,26 +4,34 @@
             [org.corfield.build :as bb]))
 
 (def lib 'com.github.dgr/clj-tda)
-(def version "0.1.0-SNAPSHOT")
+(def version "1.0.0-SNAPSHOT")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
-(defn test "Run the tests." [opts]
+(defn clean "Clean up."
+  [opts]
+  (bb/clean opts))
+
+(defn test "Run the tests."
+  [opts]
   (bb/run-tests opts))
 
-(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+(defn ci "Run the CI pipeline of tests (and build the JAR)."
+  [opts]
   (-> opts
-      (assoc :lib lib :version version)
+      (assoc :lib lib :version version :src-pom "template/pom.xml")
       (bb/run-tests)
       (bb/clean)
       (bb/jar)))
 
-(defn install "Install the JAR locally." [opts]
+(defn install "Install the JAR locally."
+  [opts]
   (-> opts
       (assoc :lib lib :version version)
       (bb/install)))
 
-(defn deploy "Deploy the JAR to Clojars." [opts]
+(defn deploy "Deploy the JAR to Clojars."
+  [opts]
   (-> opts
       (assoc :lib lib :version version)
       (bb/deploy)))
